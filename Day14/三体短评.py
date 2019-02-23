58e9e354bb39378194d70c708a12b71ecf4aa2b6
小柒2012 <345849402@qq.com>
2019-02-23 20:54:56 +0800

    我爬了《流浪地球》十万个短评得出以下结论

---
+++ Day14/三体短评.py
@@ -0,0 +1,57 @@
+import os
+# 导入requests库
+import requests
+# 导入文件操作库
+import codecs
+from bs4 import BeautifulSoup
+
+
+# 给请求指定一个请求头来模拟chrome浏览器
+global headers
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
+# 好评500条，中评500条，差评500条
+# https://movie.douban.com/subject/26266893/comments?start=0&limit=20&sort=new_score&status=P&percent_type=h
+# https://movie.douban.com/subject/26266893/comments?start=0&limit=20&sort=new_score&status=P&percent_type=m
+# https://movie.douban.com/subject/26266893/comments?start=0&limit=20&sort=new_score&status=P&percent_type=l
+server = 'https://movie.douban.com/subject/26266893/comments'
+# 定义存储位置
+global save_path
+save_path = os.getcwd()+"\\Text\\"+'短评_好评.txt'
+global page_max
+page_max = 25  # 500 条微评论，后面就看不到了，不知道是否豆瓣有意而为之给隐藏了。
+global comments
+comments = ''
+
+
+# 获取短评内容
+def get_comments(page):
+    req = requests.get(url=page)
+    html = req.content
+    html_doc = str(html, 'utf-8')
+    bf = BeautifulSoup(html_doc, 'html.parser')
+    comment = bf.find_all(class_="short")
+    for short in comment:
+        global comments
+        comments = comments + short.text
+
+
+# 写入文件
+def write_txt(chapter, content, code):
+    with codecs.open(chapter, 'a', encoding=code)as f:
+        f.write(content)
+
+
+# 主方法
+def main():
+    for i in range(0, page_max):
+        try:
+            page = server + '?start='+str(i*20)+'&limit=20&sort=new_score&status=P&percent_type=h'
+            get_comments(page)
+            write_txt(save_path, comments, 'utf8')
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
+
