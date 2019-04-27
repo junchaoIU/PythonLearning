69e49fae694c925386ff6fd91faf1d39b3aafe32
小柒2012 <345849402@qq.com>
2019-04-27 11:11:41 +0800

    :sparkles: 复仇者联盟4短评

--- Day14/三体.py
+++ Day14/三体.py
@@ -3,7 +3,7 @@ __blog__ = "https://blog.52itstyle.vip/"
 import jieba
 import os
 
-txt = open(os.getcwd()+"\\Text\\"+"短评_好评.txt", "r", encoding='utf-8').read()
+txt = open(os.getcwd()+"\\Text\\"+"复仇者联盟4短评_好评.txt", "r", encoding='utf-8').read()
 words = jieba.lcut(txt)  # 使用精确模式对文本进行分词
 counts = {}  # 通过键值对的形式存储词语及其出现的次数
 
@@ -16,7 +16,7 @@ for word in words:
 items = list(counts.items())  # 将键值对转换成列表
 items.sort(key=lambda x: x[1], reverse=True)  # 根据词语出现的次数进行从大到小排序
 
-for i in range(0, 10):
+for i in range(0, 20):
     word, count = items[i]
     print("{0:<5}{1:>5}".format(word, count))
 
