7176bddb79fc8eded6167ead6db0c96f76fe3444
小柒2012 <345849402@qq.com>
2019-10-09 20:46:50 +0800

    自动化测试

--- Day24/browser.py
+++ Day24/browser.py
@@ -8,7 +8,7 @@ browser = webdriver.Chrome()
 
 browser.maximize_window()
 
-browser.get("https://wwwbaidu.com")
+browser.get("https://www.baidu.com")
 
 browser.find_element(By.ID, "kw").send_keys("科帮网")
 
